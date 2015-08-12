09d28e193f5fce80931a9b4913dfb2ce23a1efee
Ted Li <lwxted@Teds-MacBook-Pro.local>
2015-08-12 01:33:06 -0700

    Added back ST3 configs code

--- takemehome.sh
+++ takemehome.sh
@@ -16,13 +16,13 @@ echo_status() {
 format_prompt "Setting up code environment..."
 
 # Setup Sublime Text 3 configs
-# format_prompt "Cloning Sublime Text 3 config from git repo..."
-# format_prompt "Killing all current Sublime Text instances..."
-# killall Sublime\ Text
-# echo_status
-# rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3
-# git clone --recursive git@github.com:lwxted/subl_config.git ~/Library/Application\ Support/Sublime\ Text\ 3
-# echo_status
+format_prompt "Cloning Sublime Text 3 config from git repo..."
+format_prompt "Killing all current Sublime Text instances..."
+killall Sublime\ Text
+echo_status
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3
+git clone --recursive git@github.com:lwxted/subl_config.git ~/Library/Application\ Support/Sublime\ Text\ 3
+echo_status
 
 # Sync repos
 format_prompt "Granting permissions to repo scripts and pushing to bin..."
